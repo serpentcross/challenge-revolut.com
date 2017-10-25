61d14582cb5e58df0f785f9aa64b50c90ed9fae6
Vladimir <serpentcross@mail.ru>
2017-10-25 23:29:17 +0300

    Added. Some unit tests.

--- src/test/java/com/revolut/challenge/AppTest.java
+++ src/test/java/com/revolut/challenge/AppTest.java
@@ -1,38 +1,84 @@
 package com.revolut.challenge;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.revolut.challenge.constants.EndPoints;
+import com.revolut.challenge.constants.Parameters;
+import com.revolut.challenge.constants.ServerConfig;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class AppTest {
+
+    @Before
+    public void setup() throws Exception {
+
+        RestAssured.baseURI  = ServerConfig.SERVER_HOST;
+        RestAssured.port     = ServerConfig.SERVER_PORT;
+        RestAssured.basePath = EndPoints.API;
+        RestAssured.registerParser(MediaType.TEXT_PLAIN, Parser.TEXT );
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testUserInfo1() {
+        RestAssured.given()
+                .queryParam(Parameters.ID, 1)
+                .when()
+                .get(EndPoints.CUSTOMER)
+                .then()
+                .body(containsString("9047484839"));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testUserInfo2() {
+        RestAssured.given()
+                .queryParam(Parameters.ID, 2)
+                .when()
+                .get(EndPoints.CUSTOMER)
+                .then().
+                statusCode(200);
+    }
+
+    @Test
+    public void testUserInfo3() {
+        RestAssured.given()
+                .queryParam(Parameters.ID, 10)
+                .when()
+                .get(EndPoints.CUSTOMER)
+                .then()
+                .body(containsString("Unfortunatelly, nothing was found. Please try again. Thank you."));
+    }
+
+    @Test
+    public void testTransaction1() {
+        RestAssured.given()
+                .queryParam(Parameters.SENDER_ID, 1)
+                .queryParam(Parameters.ACCOUNT_ID, 1)
+                .queryParam(Parameters.AMOUNT, 200)
+                .queryParam(Parameters.PHONE, "9224940293")
+                .when()
+                .get(EndPoints.TRANSFER)
+                .then().
+                statusCode(200);
+    }
+
+    @Test
+    public void testTransaction2() {
+        RestAssured.given()
+                .queryParam(Parameters.SENDER_ID, 1)
+                .queryParam(Parameters.ACCOUNT_ID, 1)
+                .queryParam(Parameters.AMOUNT, 20000)
+                .queryParam(Parameters.PHONE, "9769393020")
+                .when()
+                .get(EndPoints.TRANSFER)
+                .then()
+                .body(containsString("Not enough funds"));
     }
 }
